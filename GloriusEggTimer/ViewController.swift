b1dd1d2cdf58a93a5d85c00bf8e8ac26217f80c6
Alexander Kubkin <ioc0kernel@gmail.com>
2017-07-20 10:53:07 +0300

    Nill

---
+++ GloriusEggTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GloriusEggTimer
+//
+//  Created by ioc0 on 20.07.17.
+//  Copyright © 2017 web-wizards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
